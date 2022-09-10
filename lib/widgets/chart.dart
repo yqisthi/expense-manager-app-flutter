c4c9f0da5a66126bb64ab4a2c0c7033a3b2f6983
Yusuf Qisthi <yqisthi2002@gmail.com>
2022-09-10 20:55:10 +0700

    unfinished chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,18 @@
+import 'package:expense_manager_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now();
+      return {'day': 'T', 'amount': 9.99};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
